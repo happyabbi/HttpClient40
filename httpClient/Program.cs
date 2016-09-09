c2014ddefafd03d3bc19b276e1c150b070e7c510
Abraham Chen <happyabbi@gmail.com>
2016-09-09 14:06:09 +0800

    init

---
+++ httpClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+
+namespace httpClient
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			HttpClient client = new HttpClient();
+
+
+
+		}
+	}
+}
